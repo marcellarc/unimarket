9d73fc760f138f8982ccf94f36feb69fdc3cf968
Marcella <marcellaricoy@gmail.com>
2025-11-07 20:21:36 -0300

    chore: estrutura inicial do projeto (frontend + backend)

---
+++ README.md
@@ -0,0 +1,7 @@
+# UniMarket
+
+Sistema web para comparação de preços em supermercados locais.
+
+## Estrutura
+- **Frontend:** Vite + React + TypeScript + TailwindCSS
+- **Backend:** Java + Spring Boot + JPA + H2
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold text-emerald-400 ">
+        Tailwind v4 funcionando! 🌈
+      </h1>
+    </div>
+  );
+}
+
+
