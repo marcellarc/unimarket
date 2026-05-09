598e5656a1d6e5b9bbc29c0fcdf6863387ba3847
marcellarc <marcellaricoy@gmail.com>
2026-05-09 17:06:52 -0300

    fix: vercel configs para integraçao backend + front

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: '/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
