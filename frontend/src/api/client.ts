dfa3c705bab14efe7f6ac79183929c62fa21d73a
marcellarc <marcellaricoy@gmail.com>
2026-05-09 17:09:26 -0300

    fix

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const api = axios.create({
-    baseURL: '/api',
+    baseURL: '/',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
