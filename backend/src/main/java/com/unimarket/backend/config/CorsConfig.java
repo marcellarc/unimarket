20abd574e7d8961008beb8d89560f6ca5503fa23
marcellarc <marcellaricoy@gmail.com>
2026-05-09 17:27:51 -0300

    fix

--- backend/src/main/java/com/unimarket/backend/config/CorsConfig.java
+++ backend/src/main/java/com/unimarket/backend/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "http://127.0.0.1:5173"
+                                "http://127.0.0.1:5173",
+                                "https://unimarket-develop.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
