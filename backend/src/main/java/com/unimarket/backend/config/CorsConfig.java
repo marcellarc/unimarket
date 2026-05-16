ffee84fc9a5b44e7f9267a192a8d7e59765f05c0
marcellarc <marcellaricoy@gmail.com>
2026-05-16 17:55:44 -0300

    fix: variaveis de ambiente para testes em dev

--- backend/src/main/java/com/unimarket/backend/config/CorsConfig.java
+++ backend/src/main/java/com/unimarket/backend/config/CorsConfig.java
@@ -17,6 +17,9 @@ public class CorsConfig {
                         .allowedOriginPatterns(
                                 "http://localhost:*",
                                 "http://127.0.0.1:*",
+                                "https://unimarket-kappa.vercel.app",
+                                "https://unimarket-marcellarcs-projects.vercel.app",
+                                "https://unimarket-git-production-marcellarcs-projects.vercel.app",
                                 "https://unimarket-develop.vercel.app",
                                 "https://unimarket-git-develop-marcellarcs-projects.vercel.app",
                                 "https://unimarket-develop.app",
